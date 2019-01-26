6b6e5c88c3b942f2942bb017672b4e7cb9c3777b
hnspn <bobmatt911@gmail.com>
2019-01-26 17:16:02 -0500

    ressources

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private camera: Camera) {
+  }
 
+  ngOnInit(): void {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     const base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
 }
