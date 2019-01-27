fa562ece370d3d7deac1dd1eb603570b99d89141
hnspn <bobmatt911@gmail.com>
2019-01-27 04:49:35 -0500

    adding popUp

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,13 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
+
+// import { RNCamera } from 'react-native-camera';
 
 export default class App extends React.Component {
+
+   onCompost = () => {
+    Alert.alert(
+      'Composte',
+      'My Alert Msg',
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      {cancelable: false},
+    );
+  }
+
+  onRecycle = () => {
+    Alert.alert(
+      'Recycle',
+      'My Alert Msg',
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      {cancelable: false},
+    );
+  }
+
+  onTrash = () => {
+    Alert.alert(
+      'Trash',
+      'My Alert Msg',
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {
+          text: 'Cancel',
+          // color: "#000000",
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      {cancelable: false},
+    );
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Marcus mange des oranges</Text>
+        
+      <TouchableOpacity onPress = {this.onCompost} style = {styles.button}>
+         <Text>compost</Text>
+      </TouchableOpacity>
+        
+      <TouchableOpacity onPress = {this.onRecycle} style = {styles.button}>
+         <Text>Recycle</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress = {this.onTrash} style = {styles.button}>
+         <Text>Trash</Text>
+      </TouchableOpacity>
+
+      
       </View>
+    
     );
   }
 }
@@ -19,4 +89,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#000',
+    width: 100,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginTop: 100,
+ }
 });
